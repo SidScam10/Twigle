cf3fc8a2e5e076ef98189f4402b768ee03196a0f
SidScam10 <siddhanth.monnappa@gmail.com>
2025-05-11 13:03:44 +0530

    Including Firebase to Store User and Post Details

---
+++ src/firebase.jsx
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWHvfTXhOaRRdVvJzZgV3Yqm60DV7xvVU",
+  authDomain: "twigle-backend.firebaseapp.com",
+  projectId: "twigle-backend",
+  storageBucket: "twigle-backend.firebasestorage.app",
+  messagingSenderId: "412895280790",
+  appId: "1:412895280790:web:939d3be103521df87d0cb7",
+  measurementId: "G-MC4D1YH1V7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
