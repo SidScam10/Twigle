541a4377ded98d198726527a4ae8b2c6929bc464
SidScam10 <siddhanth.monnappa@gmail.com>
2025-05-12 13:35:59 +0530

    fix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,3 @@
-// src/firebaseConfig.js
 export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -6,5 +5,5 @@ export const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.end.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
